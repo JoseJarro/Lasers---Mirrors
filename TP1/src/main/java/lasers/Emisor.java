a01002bab0325c85f0e3ed88a8b9b2b43999ee07
Martin Ramos <martin.ramos1584@gmial.com>
2024-10-07 20:54:49 -0300

    refactorizacion emitirLaser

--- TP1/src/main/java/lasers/Emisor.java
+++ TP1/src/main/java/lasers/Emisor.java
@@ -41,33 +41,47 @@ public class Emisor {
         if (nivel.fueraDimension(padre.getPosicion())) {
             return;
         }
+
         var direccion = padre.getDireccion();
         for(Bloque bloque: nivel.getBloques()) {
             if (bloque.colisionaLaser(padre)) {
-                var caminos = bloque.comportamientosBloque(padre);
-                for (Vector2D camino : caminos) {
-                    if (camino == null) { continue;}
-                    if (giros.contains(padre)) {
-                        continue;
-                    }
-                    if (!(direccion.equals(padre.getDireccion())) && camino.equals(padre)) {
-                        giros.add(padre);
-                        _emitirLaser(camino, grafoLaser, nivel, giros);
-                        continue;
-                    }
-                    grafoLaser.addVertex(camino);
-                    grafoLaser.addEdge(padre, camino);
-                    _emitirLaser(camino, grafoLaser, nivel, giros);
-                }
+                colisionarBloque(padre,grafoLaser,nivel,giros,bloque,direccion);
                 return;
             }
         }
+
+        avanzarLazer(padre,grafoLaser,nivel,giros);
+    }
+
+
+    private void avanzarLazer (Vector2D padre ,Graph<Vector2D, DefaultEdge> grafoLaser,Nivel nivel, Set<Vector2D> giros) {
         Vector2D avanzar = padre.clonar();
         avanzar.moverDireccion();
         grafoLaser.addVertex(avanzar);
         grafoLaser.addEdge(padre, avanzar);
         _emitirLaser(avanzar, grafoLaser, nivel, giros);
     }
+
+
+    private void colisionarBloque(Vector2D padre, Graph<Vector2D, DefaultEdge> grafoLaser,Nivel nivel,Set<Vector2D> giros, Bloque bloque, String direccion){
+
+        var caminos = bloque.comportamientosBloque(padre);
+        for (Vector2D camino : caminos) {
+            if (camino == null) { continue;}
+            if (giros.contains(padre)) {
+                continue;
+            }
+            if (!(direccion.equals(padre.getDireccion())) && camino.equals(padre)) {
+                giros.add(padre);
+                _emitirLaser(camino, grafoLaser, nivel, giros);
+                continue;
+            }
+            grafoLaser.addVertex(camino);
+            grafoLaser.addEdge(padre, camino);
+            _emitirLaser(camino, grafoLaser, nivel, giros);
+        }
+    }
+
 
     @Override
     public String toString() {
